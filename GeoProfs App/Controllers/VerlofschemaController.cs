c4c3135ca9942f18fd2e31ec1d1edb8911a80f19
JulianW051 <julianwessels1@gmail.com>
2023-06-21 15:54:13 +0200

    verlofschema front end

---
+++ GeoProfs App/Controllers/VerlofschemaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeoProfs_App.Controllers
+{
+    public class VerlofschemaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
