99a5cc817bd087f0e0b97292e3e62f5816319c59
TheunissenSil <theunissensil@gmail.com>
2023-11-02 10:18:37 +0100

    Final

--- GeoProfs App/Controllers/IctController.cs
+++ GeoProfs App/Controllers/IctController.cs
@@ -6,6 +6,7 @@ using GeoProfs_App.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace GeoProfs_App.Controllers
 {
@@ -25,6 +26,47 @@ namespace GeoProfs_App.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Backup()
+        {
+            try
+            {
+                string backupFileName = "GeoProfs_Backup_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".sql";
+
+                string connectionString = "Server=localhost;Port=3306;Database=GeoProfs;Uid=root;";
+
+                string backupFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, backupFileName);
+
+                string mysqldumpPath = @"C:\xampp\mysql\bin\mysqldump.exe";
+
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = mysqldumpPath;
+                    process.StartInfo.Arguments = $"--host=localhost --port=3306 --user=root GeoProfs --result-file=\"{backupFilePath}\"";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                // Return the backup file for download.
+                var stream = new FileStream(backupFilePath, FileMode.Open);
+                return File(stream, "application/octet-stream", backupFileName);
+            }
+            catch (Exception ex)
+            {
+                // Log the error to the console
+                Console.WriteLine($"An error occurred during the backup process: {ex.ToString()}");
+
+                var errorModel = new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                    ErrorMessage = ex.Message,
+                };
+
+                return View("Error", errorModel);
+            }
         }
 
         // Action to list users
@@ -110,95 +152,8 @@ namespace GeoProfs_App.Controllers
                 ModelState.AddModelError(string.Empty, error.Description);
             }
 
-
-        public IActionResult Backup()
-        {
-            try
-            {
-                string backupFileName = "GeoProfs_Backup_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".sql";
-
-                string connectionString = "Server=localhost;Port=3306;Database=GeoProfs;Uid=root;";
-
-                string backupFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, backupFileName);
-
-                string mysqldumpPath = @"C:\xampp\mysql\bin\mysqldump.exe"; 
-
-                using (var process = new Process())
-                {
-                    process.StartInfo.FileName = mysqldumpPath; 
-                    process.StartInfo.Arguments = $"--host=localhost --port=3306 --user=root GeoProfs --result-file=\"{backupFilePath}\"";
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.Start();
-                    process.WaitForExit();
-                }
-
-                // Return the backup file for download.
-                var stream = new FileStream(backupFilePath, FileMode.Open);
-                return File(stream, "application/octet-stream", backupFileName);
-            }
-            catch (Exception ex)
-            {
-                // Log the error to the console
-                Console.WriteLine($"An error occurred during the backup process: {ex.ToString()}");
-
-                var errorModel = new ErrorViewModel
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
-                    ErrorMessage = ex.Message,
-                };
-
-                return View("Error", errorModel);   
-            }
-        }
-
-        [HttpGet]
-        public IActionResult CreateUser()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateUserPost()
-        {
-            string userName = Request.Form["UserName"];
-            string email = Request.Form["Email"];
-            string password = Request.Form["Password"];
-            string confirmPassword = Request.Form["ConfirmPassword"];
-
-            // Now you have the form values, and you can proceed with user creation logic.
-
-            // Example: Create a new ApplicationUser and set its properties
-            var user = new ApplicationUser
-            {
-                UserName = userName,
-                Email = email,
-                // Set other user properties as needed
-            };
-
-            // Create the user using UserManager
-            var result = await _userManager.CreateAsync(user, password);
-
-            if (result.Succeeded)
-            {
-                // User created successfully
-                // You can redirect to a success page or perform other actions
-                return RedirectToAction("UserList");
-            }
-
-            // If user creation fails, add errors to ModelState and redisplay the form
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
-
             // If the model is not valid or user creation fails, redisplay the form
             return View("CreateUser");
         }
-
-
-
-
-
-        }
     }
+}
